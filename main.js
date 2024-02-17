320cf331b2034ed52ebbe061f2c5c1d88b980966
moritz <moritz.hesse@cheddarps.com>
2024-02-17 09:14:31 +0000

    Add basic cube

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -8,11 +9,17 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ffaa } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
 camera.position.z = 5;
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
 
 function animate() {
 	requestAnimationFrame( animate );
@@ -21,6 +28,17 @@ function animate() {
 	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
+}
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
 }
 
 animate();
