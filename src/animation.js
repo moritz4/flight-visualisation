69a6ec709b20be01be6ae8daad3531fbd26ba5d2
moritz <moritz.hesse@cheddarps.com>
2024-02-26 22:12:59 +0000

    Add moving points

--- src/animation.js
+++ src/animation.js
@@ -6,9 +6,11 @@ import {unix, format} from 'dayjs'
 import {linear} from "everpolate";
 import {flights} from "/src/main"
 
+const TRAILLENGTH = 1200; // trail length in seconds
+
 // Create clock object to keep time
 export const clock = {
-    step: 1000, // 1 = real time, 2 = x2, 3 = x3 etc.
+    step: 100, // 1 = real time, 2 = x2, 3 = x3 etc.
     t: 0, 
     start: Infinity, 
     stop:0, 
@@ -49,9 +51,17 @@ export function initFlights(flights) {
         // Add curve object (visual representation of the curve)
         const points = curve.getPoints( 300 );
         const geometry = new THREE.BufferGeometry().setFromPoints( points );
-        const material = new THREE.LineBasicMaterial( { color: 0xff0000, transparent: true, opacity: 0.2 } );
+        const material = new THREE.LineBasicMaterial( { color: 0xff0000, transparent: true, opacity: 0.5 } );
         const curveObject = new THREE.Line( geometry, material );
-        flights[key]["curveObject"] = curveObject
+        flights[key]["curveObject"] = curveObject;
+
+        // Add point object (visual representation of the plane's current position)
+        const dotGeometry = new THREE.BufferGeometry();
+        dotGeometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array([curve.getPoint(0).x, curve.getPoint(0).y, curve.getPoint(0).z]), 3));
+        dotGeometry.dynamic = true;
+        const dotMaterial = new THREE.PointsMaterial({ size: 10, color: 0xffffff });
+        const dot = new THREE.Points(dotGeometry, dotMaterial);
+        flights[key]["point"] = dot;
 
         //Add an indiator for whether the point of the plane has been added or not
         flights[key]["pointAdded"] = false;
@@ -60,6 +70,20 @@ export function initFlights(flights) {
 
     // Update the clock again to set the current time to the start time
     clock.t = clock.start
+}
+
+// Function to get plane position at a time t
+function getPosition(key, t) {
+    // if plane is not in the air, it doesnt have a position yet
+    if ((t < flights[key].start || t > (flights[key].stop))) {
+        return(undefined);
+    }
+
+    // Else, use everpolate to get the u position along the curve.
+    // x is time and y is u. To create u we need an evenly spaced vector from 0 to 1
+    let linspace = Array.from({length: flights[key].time.length}, (v, i) => i/(flights[key].time.length - 1))
+    const u = linear(t, flights[key].time, linspace)[0]
+    return(flights[key].curve.getPoint(u))
 }
 
 // Function which is run once at the start
@@ -75,24 +99,51 @@ export function animateScene(scene) {
     // Loop through all flights
     for (var key of Object.keys(flights)) {
         animateLine(scene, key);
+        animatePoint(scene, key);
     }
 }
 
 function animateLine(scene, key) {
 
     // Add line of the flightpath if it needs to be added
-    if (flights[key].curveAdded == false && clock.t >= flights[key].start && clock.t <= flights[key].stop) {
+    if (flights[key].curveAdded == false && clock.t >= flights[key].start && clock.t <= (flights[key].stop + TRAILLENGTH)) {
         scene.add(flights[key].curveObject);
         flights[key].curveAdded = true;   
     }
 
     // Remove line
-    if (flights[key].curveAdded == true && (clock.t < flights[key].start || clock.t > flights[key].stop)) {
+    if (flights[key].curveAdded == true && (clock.t < flights[key].start || clock.t > (flights[key].stop + TRAILLENGTH))) {
         scene.remove(flights[key].curveObject)
         flights[key].curveAdded = false;
     }
 }
 
+function animatePoint(scene, key) {
+
+    // Add line of the flightpath if it needs to be added
+    if (flights[key].pointAdded == false && clock.t >= flights[key].start && clock.t <= (flights[key].stop)) {
+        scene.add(flights[key].point);
+        flights[key].pointAdded = true;  
+        return; 
+    }
+
+    // Remove point
+    if (flights[key].pointAdded == true && (clock.t < flights[key].start || clock.t > (flights[key].stop))) {
+        scene.remove(flights[key].point)
+        flights[key].pointAdded = false;
+        return;
+    }
+
+    // Move point
+    if (clock.t >= flights[key].start && clock.t <= flights[key].stop) {
+        const pos = getPosition(key, clock.t)
+        flights[key].point.geometry.attributes.position.array[0] = pos.x;
+        flights[key].point.geometry.attributes.position.array[1] = pos.y;
+        flights[key].point.geometry.attributes.position.array[2] = pos.z;
+        flights[key].point.geometry.attributes.position.needsUpdate = true;
+    }
+}
+
 document.getElementById("button").addEventListener("click", () => {
-    console.log("running debug")
+    console.log(getPosition(Object.keys(flights)[1], 1708895458))
 });
